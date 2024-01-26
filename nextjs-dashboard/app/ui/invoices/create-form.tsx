0d0972a2dda612c6cbdc953732aeffd3638c7fe9
ailenrmansilla <ailen.r.mansilla@gmail.com>
2024-01-26 01:55:07 -0500

    chapter 14

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -7,10 +7,15 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
+      {/*  in React, the action attribute is considered a special prop 
+      meaning React builds on top of it to allow actions to be invoked. */}
+      
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
